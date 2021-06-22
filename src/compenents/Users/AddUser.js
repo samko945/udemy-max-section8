201669beff0cf7e5aa2314fcfd073893bac72baf
Samuel <samkoj@hotmail.co.uk>
2021-06-22 22:23:25 +0100

    state for AddUser form values

--- src/compenents/Users/AddUser.js
+++ src/compenents/Users/AddUser.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 export default function AddUser() {
+	const [enteredUsername, setEnteredUsername] = useState("");
+	const [enteredAge, setEnteredAge] = useState("");
+	function usernameChangeHandler(event) {
+		const newValue = event.target.value;
+		setEnteredUsername(newValue);
+	}
+	function ageChangeHandler(event) {
+		const newValue = event.target.value;
+		setEnteredAge(newValue);
+	}
 	function addUserHandler(event) {
 		event.preventDefault();
+		console.log(enteredUsername, enteredAge);
 	}
 	return (
 		<Card className={classes.input}>
 			<form onSubmit={addUserHandler}>
 				<label htmlFor="username">Username</label>
-				<input id="username" type="text" />
+				<input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
 				<label htmlFor="age">Age</label>
-				<input id="age" type="number" />
+				<input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
 				<Button type="submit">Submit</Button>
 			</form>
 		</Card>
